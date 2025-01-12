59581a4f16f18ccb8f14fbd1697d2c9c98cbf61c
el-arma <>
2025-01-12 14:38:09 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  let title = 'Wall-E';
+
+  const [title, setTitle] = useState('Wall-E');
+
+  let message;
+    if (title.length < 5) {
+        message = "Tytuł jest za krótki. Nagrywają takie filmy?";
+    } else if (title.length < 15) {
+        message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+    } else {
+        message = "Tytuł jest za długi, nikt tego nie zapamięta.";
+    }
+
+    {
+        title.length > 0 && <div>{message}</div>
+    }
 
   function handleChange(event) {
-      console.log(event.target.value);
+
+      setTitle(event.target.value);
+
   }
 
+
   return (
+
       <div>
+
           <h1>My favourite movies to watch</h1>
+
           <h2>My favourite movie for today is {title}</h2>
-          <input type="text" onChange={handleChange}/>
+
+          <br></br>
+
+          {title.length > 0 && <div>{message}</div>}
+
+          <input type="text" value={title} onChange={handleChange}/>
+
       </div>
+
   );
+
 }
 
 export default App;
