9b43318ebe1b418a5b4e78ef28d11ad11c81eb0b
el-arma <>
2025-01-25 14:15:54 +0100

    Decompostion complete

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import './App.css';
 import {useState} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
+import MoviesList from "./MoviesList";
 
 function App() {
-    // const [title, setTitle] = useState('');
 
     const [movies, setMovies] = useState([]);
 
     return (
         <div className="container">
             <h1>My favourite movies to watch</h1>
-            <h2>Titles</h2>
-            <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
-            </ul>
-            <h2>Add movie</h2>
+            <h2>Titles:</h2>
+
 
             <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
-
-          
+            <MoviesList movies={movies}/>
 
         </div>
     );
 }
 
 export default App;
+
+
